3c1be5243ce4e94ac8b0a6c89421bc7473e669a0
Jordan R <jaygerzero@gmail.com>
2019-12-11 18:45:20 -0500

    Prims vs Objs 2

--- script.js
+++ script.js
@@ -49,6 +49,7 @@ console.log(sun.user);
  */
 
 // Object.create
+/*
 var personProto = {
     calculateAge: function() {
         console.log(2019 - this.yearOfBirth);
@@ -65,3 +66,42 @@ var jane = Object.create(personProto, {
     yearOfBirth: { value: 1969 },
     job: { value: 'designer' }
 })
+*/
+
+// Primitives vs Objects
+
+
+//Primitives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+
+// Objects
+var obj1 = {
+    name: 'John',
+    age: 29
+};
+
+var obj2 = obj1;
+obj1.age = 32;
+console.log(obj1);
+console.log(obj2)
+
+// Functions
+var age = 27;
+var obj = {
+    name: 'Jonas',
+    city: 'Lisbon'
+}
+
+function change(a, b) {
+    a = 30;
+    b.city = 'San Francisco';
+}
+
+change(age, obj);
+console.log(age);
+console.log(obj.city)
