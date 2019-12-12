46c512cdf32e228e5983aecc629f717cf32bbe59
Jordan R <jaygerzero@gmail.com>
2019-12-11 20:47:47 -0500

    IIFE

--- script.js
+++ script.js
@@ -174,3 +174,22 @@ designerQuestion('Mike');
 interviewQuestion('teacher')('Mark');
 */
 
+// Lecture: IIFE
+/*
+function game() {
+    var score = Math.random() * 10;
+    console.log(score >= 5);
+}
+
+game();
+*/
+
+(function() {
+    var score = Math.random() * 10;
+    console.log(score >= 5);
+})();
+
+(function(goodLuck) {
+    var score = Math.random() * 10;
+    console.log(score >= 5 - goodLuck);
+})(5);
