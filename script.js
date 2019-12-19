1b081579827e781078b7371cd68e995490f1aa7f
Jordan R <jaygerzero@gmail.com>
2019-12-18 22:02:17 -0500

    CC#7

--- script.js
+++ script.js
@@ -302,3 +302,83 @@ var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
 console.log(ages);
 console.log(fullJapan);
 */
+
+
+// Coding Challenge #7
+/*
+(function() {
+
+function Question(question, answers, correct) {
+    this.question = question;
+    this.answers = answers;
+    this.correct = correct;
+}
+
+Question.prototype.displayQuestion = 
+    function() {
+        console.log(this.question);
+
+        for(var i = 0; i < this.answers.length; i++) {
+            console.log(i + ': ' + this.answers[i]);
+        }
+};
+
+Question.prototype.checkAnswer = 
+    function(ans, callback) {
+        var sc;
+
+        if(ans === this.correct) {
+            console.log('Correct Answer')
+            sc = callback(true);
+        } else {
+            console.log('Wrong answer. Try Again: ')
+            sc = callback(false);
+        }
+        this.displayScore(sc);
+};
+
+Question.prototype.displayScore = 
+    function(score) {
+        console.log('Your currenct score is:' + score);
+        console.log('----------------------------');
+
+}
+
+
+var question1 = new Question('Is JavaScript the coolest programming language in the world?', ['Yes', 'No'], 0);
+
+var question2 = new Question('What is the name of this course\'s teacher?', ['John', 'Michael', 'Jonas'], 2);
+
+var question3 = new Question('What does best describe coding?', ['Boring', 'Hard', 'Fun', 'Tedious'], 2);
+
+var questions = [question1, question2, question3];
+
+function score() {
+    var sc = 0;
+    return function(correct) {
+        if (correct) {
+            sc++;
+        } 
+        return sc;
+    }
+};
+
+var keepScore = score();
+
+function nextQuestion(){
+    var n = Math.floor(Math.random() * questions.length);
+    questions[n].displayQuestion();
+
+    var answer = prompt('Please select the correct answer.');
+
+    if(answer !== 'exit') {
+        questions[n].checkAnswer(parseInt(answer), keepScore);
+
+        nextQuestion();
+    }
+};
+
+nextQuestion();
+
+})();
+*/
